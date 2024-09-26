24cec7b1a66e9f4cd24abd21f56232522b078bf5
Your Name <you@example.com>
2024-09-26 17:08:37 +0800

    add flatten

--- flatten.js
+++ flatten.js
@@ -1,3 +1,19 @@
 /**
- * Implement flatten
+ * Implement flatten nested array
  */
+
+// 2 ways
+
+// first way: recursion
+function flattenArray(arr) {
+    return arr.reduce((acc, val) => {
+        if (Array.isArray(val)) {
+            return acc.concat(flattenArray(val))
+        } 
+        return acc.concat(val)
+    }, [])
+}
+
+const nestedArray = [1, [2, 3], [4, [5, 6]]];
+const flattenedArray = flattenArray(nestedArray);
+console.log(flattenedArray);  // Output: [1, 2, 3, 4, 5, 6]
