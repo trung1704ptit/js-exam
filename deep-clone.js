9acd61d1e5005c91b6486c96db26a3417b58ee6f
Your Name <you@example.com>
2024-10-05 13:49:09 +0800

    add deep clone

---
+++ deep-clone.js
@@ -0,0 +1,30 @@
+/**
+ * Here’s an implementation of deep cloning in JavaScript, which creates a new copy of an object,
+ * including all nested objects or arrays, ensuring that modifying the clone does not affect the original object.
+ */
+
+
+// A WeakMap is used here because it allows garbage collection of its keys when they are no longer referenced, avoiding memory leaks.
+
+function deepClone(obj, hash = new WeakMap()) {
+    if (obj === null || typeof obj !== "object") {
+        return obj; // obj is value, then return it
+    }
+
+    if (hash.has(obj)) { // checking Circular References
+        return hash.get(obj)
+    }
+
+    const clonedObj = Array.isArray(obj) ? [] : {}
+
+    // Store the clone in the Weakmap
+    hash.set(obj, clonedObj)
+    
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            clonedObj[key] = deepClone(obj[key], hash)
+        }
+    }
+
+    return clonedObj;
+}
