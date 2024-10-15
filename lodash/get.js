e4d93f89399eac7e8bb5b33db342f1259c860ceb
Trung Nguyen <trung1704ptit@gmail.com>
2024-10-15 23:34:46 +0800

    update get

--- lodash/get.js
+++ lodash/get.js
@@ -1,17 +1,37 @@
+/*
+implement again get method
+
+var object = { 'a': [{ 'b': { 'c': 3 } }] };
+ 
+_.get(object, 'a[0].b.c');
+// => 3
+ 
+_.get(object, ['a', '0', 'b', 'c']);
+// => 3
+ 
+_.get(object, 'a.b.c', 'default');
+// => 'default'
+*/
+
+/// path = "a[0].b.c" => idea: split it in to list: [a, 0, b, c]  objec["a"] => got it, if number obj["a"][0]
+
 function get(obj, path, defaultVal) {
-  // obj is object, and path is string
-  let current = obj;
+  // if path is string, we need to handle both dot and bracket notation
+  if (typeof path === "string") {
+    path = path.replace(/\[(\d+)]/g, '.$1').split(".")
+  }
+  let result = obj
 
-  const keys = path.split("."); // an array
+  for (let i =0; i< path.length; i ++) {
+    const key =  path[i]
 
-  for (let key of keys) {
-    // Check if the key exists in the current object
-    if (current && key in current) {
-      current = current[key]; // Update current to the next nested value
-    } else {
-      return defaultVal; // Return the default value if key does not exist
+    const keyAsNumber = isNaN(key) ? key : Number(key);
+
+    result = result ? result[keyAsNumber] : undefined;
+
+    if (result === undefined) {
+      return defaultVal;
     }
   }
-
-  return current;
+  return result;
 }
