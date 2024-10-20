8584d07bf6c617799497d60346821b2e84a5407b
Trung Nguyen <trung1704ptit@gmail.com>
2024-10-20 12:26:20 +0800

    seconda way deepclone

--- lodash/deep-clone.js
+++ lodash/deep-clone.js
@@ -3,28 +3,33 @@
  * including all nested objects or arrays, ensuring that modifying the clone does not affect the original object.
  */
 
-
 // A WeakMap is used here because it allows garbage collection of its keys when they are no longer referenced, avoiding memory leaks.
 
 function deepClone(obj, hash = new WeakMap()) {
-    if (obj === null || typeof obj !== "object") {
-        return obj; // obj is value, then return it
+  if (obj === null || typeof obj !== "object") {
+    return obj; // obj is value, then return it
+  }
+
+  if (hash.has(obj)) {
+    // checking Circular References
+    return hash.get(obj);
+  }
+
+  const clonedObj = Array.isArray(obj) ? [] : {};
+
+  // Store the clone in the Weakmap
+  hash.set(obj, clonedObj);
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      clonedObj[key] = deepClone(obj[key], hash);
     }
+  }
 
-    if (hash.has(obj)) { // checking Circular References
-        return hash.get(obj)
-    }
+  return clonedObj;
+}
 
-    const clonedObj = Array.isArray(obj) ? [] : {}
-
-    // Store the clone in the Weakmap
-    hash.set(obj, clonedObj)
-    
-    for (let key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            clonedObj[key] = deepClone(obj[key], hash)
-        }
-    }
-
-    return clonedObj;
+// ------------------ other way ----------------------------
+function deepClone2(obj) {
+  return JSON.parse(JSON.stringify(obj));
 }
